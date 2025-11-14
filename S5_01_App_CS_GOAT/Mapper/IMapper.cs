415b30a569a9ece4776f6c09cff2e05f32ed1341
Ewan-Ponchaud <155959234+Ewan-Ponchaud@users.noreply.github.com>
2025-11-14 14:46:07 +0100

    feat: ajout de logique dans IMapper.cs et IDataRepository.cs
    - Création de l’interface IMapper
    - Ajout des méthodes dans IDataRepository
    - Préparation pour la prochaine étape d’intégration des données

--- S5_01_App_CS_GOAT/Mapper/IMapper.cs
+++ S5_01_App_CS_GOAT/Mapper/IMapper.cs
@@ -1,6 +1,17 @@
-﻿namespace S5_01_App_CS_GOAT.Mapper
+﻿namespace S5_01_App_CS_GOAT.Mapper;
+
+public interface IMapper<Entity, DTO>
 {
-    public interface IMapper
+    Entity ToEntity(DTO dto);
+    DTO ToDTO(Entity entity);
+
+    IEnumerable<Entity> ToEntity(IEnumerable<DTO> entities)
     {
+        return entities.Select(e => ToEntity(e));
+    }
+
+    IEnumerable<DTO> ToDTO(IEnumerable<Entity> entities)
+    {
+        return entities.Select(e => ToDTO(e));
     }
 }
--- S5_01_App_CS_GOAT/Models/Repository/IDataRepository.cs
+++ S5_01_App_CS_GOAT/Models/Repository/IDataRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace S5_01_App_CS_GOAT.Models.Repository
+﻿namespace S5_01_App_CS_GOAT.Models.Repository;
+
+public interface ISearchableRepository<TEntity, TKey>
 {
-    public interface IDataRepository
-    {
-    }
+    Task<TEntity> GetByKeyAsync(TKey key);
 }
+
+public interface IReadableRepository<TEntity, TIdentifier>
+{
+    Task<IEnumerable<TEntity?>> GetAllAsync();
+    Task<TEntity?> GetByIdAsync(TIdentifier id);
+}
+
+public interface IWriteRepository<TEntity>
+{
+    Task<TEntity> AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entityToUpdate, TEntity entity);
+    Task DeleteAsync(TEntity entity);
+}
+
+public interface IDataRepository <TEntity, TIdentifier, TKey>
+    : ISearchableRepository<TEntity, TKey>, IReadableRepository<TEntity, TIdentifier>, IWriteRepository<TEntity>
+{ }
