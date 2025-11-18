e408ed829e79873260f851fae1e498c7197e608e
LegAcyMDN <ar9legacy.main@gmail.com>
2025-11-18 17:45:40 +0100

    Création du Controller pour la classe Ban. Transformation du User en UserDTO, ainsi que la création du mapper User To UserDTO et UserDTO to User.

--- S5_01_App_CS_GOAT/DTO/UserDTO.cs
+++ S5_01_App_CS_GOAT/DTO/UserDTO.cs
@@ -1,6 +1,26 @@
-﻿namespace S5_01_App_CS_GOAT.DTO
+﻿namespace S5_01_App_CS_GOAT.DTO;
+
+public class UserDTO
 {
-    public class UserDTO
-    {
-    }
+    public string Login { get; set; } = null!;
+
+    public string DisplayName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public DateTime? PhoneVerifiedOn { get; set; }
+
+    public DateTime? EmailVerifiedOn { get; set; }
+
+    public bool TwoFaIsPhone { get; set; }
+
+    public bool TwoFaIsEmail { get; set; }
+
+    public DateTime CreationDate { get; set; }
+
+    public string Seed { get; set; } = null!;
+
+    public double Wallet { get; set; }
 }
