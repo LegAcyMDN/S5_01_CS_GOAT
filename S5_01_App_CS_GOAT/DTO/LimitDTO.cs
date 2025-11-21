7efa750aa8e3c0e7f1f5b5fe4ea54972d6684af6
millerob <millerob@etu.univ-smb.fr>
2025-11-21 09:23:46 +0100

    Modification des DTO (USER ignoré)

--- S5_01_App_CS_GOAT/DTO/LimitDTO.cs
+++ S5_01_App_CS_GOAT/DTO/LimitDTO.cs
@@ -3,5 +3,9 @@
     public class LimitDTO
     {
         public double LimitAmount { get; set; } = 0;
+
+        public string LimitTypeName { get; set; } = null!;
+
+        public string DurationName { get; set; } = null!;
     }
 }
