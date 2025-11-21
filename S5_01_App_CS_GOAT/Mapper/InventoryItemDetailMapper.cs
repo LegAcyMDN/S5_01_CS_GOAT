2533525a265d2aeb05cd51a0e5bdd53d64649471
Rselmane <rayan.selmane@etu.univ-smb.fr>
2025-11-21 09:34:29 +0100

    InventoryItemMapper

--- S5_01_App_CS_GOAT/Mapper/InventoryItemDetailMapper.cs
+++ S5_01_App_CS_GOAT/Mapper/InventoryItemDetailMapper.cs
@@ -4,20 +4,30 @@ using S5_01_App_CS_GOAT.Models.EntityFramework;
 
 namespace S5_01_App_CS_GOAT.AutoMapper;
 
-public class InventoryItemDetail : Profile
+public class InventoryItemDetailMapper : Profile
 {
-    public InventoryItemDetail()
+    public InventoryItemDetailMapper()
     {
-        // Entity → DTO
-        CreateMap<InventoryItem, InventoryItemDTO>()
+        // Entity -> DTO
+        CreateMap<InventoryItem, InventoryItemDetailDTO>()
+            .ForMember(dest => dest.Float, opt => opt.MapFrom(src => src.Float))
+            .ForMember(dest => dest.IsFavorite, opt => opt.MapFrom(src => src.IsFavorite))
+            .ForMember(dest => dest.AcquiredOn, opt => opt.MapFrom(src => src.AcquiredOn))
+            .ForMember(dest => dest.Uuid, opt => opt.MapFrom(src => src.Wear.Uuid))
+            .ForMember(dest => dest.WearName, opt => opt.MapFrom(src => src.Wear.WearName))
+            .ForMember(dest => dest.SkinName, opt => opt.MapFrom(src => src.Wear.Skin.SkinName))
+            .ForMember(dest => dest.RarityColor, opt => opt.MapFrom(src => src.Wear.Skin.Rarity.RarityColor))
+            .ForMember(dest => dest.RarityName, opt => opt.MapFrom(src => src.Wear.Skin.Rarity.RarityName))
+            .ForMember(dest => dest.ItemName, opt => opt.MapFrom(src => src.Wear.Skin.Item.ItemName))
+            .ForMember(dest => dest.ItemTypeName, opt => opt.MapFrom(src => src.Wear.Skin.Item.ItemType.ItemTypeName));
+
+        // DTO -> Entity
+        CreateMap<InventoryItemDetailDTO, InventoryItem>()
+            .ForMember(dest => dest.UserId, opt => opt.Ignore())
+            .ForMember(dest => dest.WearId, opt => opt.Ignore())
+            .ForMember(dest => dest.RemovedOn, opt => opt.Ignore())
             .ForMember(dest => dest.Float, opt => opt.MapFrom(src => src.Float))
             .ForMember(dest => dest.IsFavorite, opt => opt.MapFrom(src => src.IsFavorite))
             .ForMember(dest => dest.AcquiredOn, opt => opt.MapFrom(src => src.AcquiredOn));
-
-        // DTO → Entity
-        CreateMap<InventoryItemDTO, InventoryItem>()
-            .ForMember(dest => dest.UserId, opt => opt.Ignore())
-            .ForMember(dest => dest.WearId, opt => opt.Ignore())
-            .ForMember(dest => dest.RemovedOn, opt => opt.Ignore());
     }
 }
