62df6fc8206c7742da7f6ce63f924b85a65870e5
Rselmane <rayan.selmane@etu.univ-smb.fr>
2025-11-21 11:39:50 +0100

    ItemTransaction

---
+++ S5_01_App_CS_GOAT/DTO/ItemTransactionDTO.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using S5_01_App_CS_GOAT.Models.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace S5_01_App_CS_GOAT.DTO
+{
+    public class ItemTransactionDTO
+    {
+       public DateTime TransactionDate { get; set; }
+        public double WalletValue { get; set; }
+
+        public DateTime? CancelledOn { get; set; }
+    }
+        
+}
+         
+
+
+
