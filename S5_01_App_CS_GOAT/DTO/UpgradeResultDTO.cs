54e26ef53b71bebe512ad2e61ae6f0762cbb0a42
Rselmane <rayan.selmane@etu.univ-smb.fr>
2025-11-21 17:17:10 +0100

    UpgradeResult DTO + Mapper

---
+++ S5_01_App_CS_GOAT/DTO/UpgradeResultDTO.cs
@@ -0,0 +1,32 @@
+﻿namespace S5_01_App_CS_GOAT.DTO
+{
+    public class UpgradeResultDTO
+    {
+        public float FloatStart { get; set; }
+
+
+        public float FloatEnd { get; set; }
+
+
+        public double ProbIntact { get; set; }
+
+
+        public double ProbDegrade { get; set; }
+
+
+        public double PropDestroy { get; set; }
+
+
+        public string DegradeFunction { get; set; } = null!;
+
+        public int FairRandomId { get; set; }
+
+        public int? UserIdUpgrade { get; set; }
+
+        public int? WearIdUpgrade { get; set; }
+
+        public int TransactionId { get; set; }
+
+
+    }
+}
---
+++ S5_01_App_CS_GOAT/Mapper/UpgradeResultMapper.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using S5_01_App_CS_GOAT.DTO;
+using S5_01_App_CS_GOAT.Models.EntityFramework;
+
+namespace S5_01_App_CS_GOAT.Mapper
+{
+    public class UpgradeResultMapper : Profile
+    {
+        public UpgradeResultMapper()
+        {
+            // Entity -> DTO
+            CreateMap<UpgradeResult, UpgradeResultDTO>()
+                .ForMember(dest => dest.FloatStart, opt => opt.MapFrom(src => src.FloatStart))
+                .ForMember(dest => dest.FloatEnd, opt => opt.MapFrom(src => src.FloatEnd))
+                .ForMember(dest => dest.ProbIntact, opt => opt.MapFrom(src => src.ProbIntact))
+                .ForMember(dest => dest.ProbDegrade, opt => opt.MapFrom(src => src.ProbDegrade))
+                .ForMember(dest => dest.PropDestroy, opt => opt.MapFrom(src => src.PropDestroy))
+                .ForMember(dest => dest.DegradeFunction, opt => opt.MapFrom(src => src.DegradeFunction))
+                .ForMember(dest => dest.UserIdUpgrade, opt => opt.MapFrom(src => src.UserId))
+                .ForMember(dest => dest.WearIdUpgrade, opt => opt.MapFrom(src => src.WearId))
+                .ForMember(dest => dest.FairRandomId, opt => opt.MapFrom(src => src.FairRandoms.First().FairRandomId))
+                .ForMember(dest => dest.TransactionId, opt => opt.MapFrom(src => src.RandomTransactions.First().TransactionId));
+
+            // DTO -> Entity 
+            CreateMap<UpgradeResultDTO, UpgradeResult>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserIdUpgrade))
+                .ForMember(dest => dest.WearId, opt => opt.MapFrom(src => src.WearIdUpgrade))
+                .ForMember(dest => dest.FloatStart, opt => opt.MapFrom(src => src.FloatStart))
+                .ForMember(dest => dest.FloatEnd, opt => opt.MapFrom(src => src.FloatEnd))
+                .ForMember(dest => dest.ProbIntact, opt => opt.MapFrom(src => src.ProbIntact))
+                .ForMember(dest => dest.ProbDegrade, opt => opt.MapFrom(src => src.ProbDegrade))
+                .ForMember(dest => dest.PropDestroy, opt => opt.MapFrom(src => src.PropDestroy))
+                .ForMember(dest => dest.DegradeFunction, opt => opt.MapFrom(src => src.DegradeFunction))
+                .ForMember(dest => dest.FairRandoms, opt => opt.MapFrom((src, dest) =>
+                    (src.FairRandomId != 0) ? new List<FairRandom> { new FairRandom { FairRandomId = src.FairRandomId } } : new List<FairRandom>()))
+                .ForMember(dest => dest.RandomTransactions, opt => opt.MapFrom((src, dest) =>
+                    (src.TransactionId != 0) ? new List<RandomTransaction> { new RandomTransaction { TransactionId = src.TransactionId } } : new List<RandomTransaction>()));
+        }
+    }
+}
