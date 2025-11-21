601340f11d9c39f2b31a7adf6965ce78bb9f2399
millerob <millerob@etu.univ-smb.fr>
2025-11-21 10:34:02 +0100

    Création du DTO détail user (avec enum)

---
+++ S5_01_App_CS_GOAT/DTO/UserDetailDTO.cs
@@ -0,0 +1,28 @@
+﻿using S5_01_App_CS_GOAT.Models.Partials;
+
+namespace S5_01_App_CS_GOAT.DTO;
+
+public class UserDetailDTO
+{
+    public string Login { get; set; } = null!;
+
+    public string DisplayName { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public bool PhoneIsVerified { get; set; }
+
+    public bool EmailIsVerified { get; set; }
+
+    public TwoFAmethod TwoFA { get; set; }
+
+    public DateTime CreationDate { get; set; }
+
+    public string Seed { get; set; } = null!;
+
+    public double Wallet { get; set; }
+
+    public bool IsSteamLogin { get; set; }
+}
