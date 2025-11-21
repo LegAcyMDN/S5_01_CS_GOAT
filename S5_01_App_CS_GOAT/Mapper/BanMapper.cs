93d85d467faf598d25143c342bad67a1cf51da96
Rselmane <rayan.selmane@etu.univ-smb.fr>
2025-11-21 15:45:12 +0100

    Ban Mapper

---
+++ S5_01_App_CS_GOAT/Mapper/BanMapper.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using S5_01_App_CS_GOAT.DTO;
+using S5_01_App_CS_GOAT.Models.EntityFramework;
+
+namespace S5_01_App_CS_GOAT.Mapper
+{
+    public class BanMapper : Profile
+    {
+        public BanMapper()
+        {
+            // Entity -> DTO
+            CreateMap<Ban, BanDTO>()
+                .ForMember(dest => dest.BanReason, opt => opt.MapFrom(src => src.BanReason))
+                .ForMember(dest => dest.BanDate, opt => opt.MapFrom(src => src.BanDate))
+                .ForMember(dest => dest.BanDuration, opt => opt.MapFrom(src => src.BanDuration))
+                .ForMember(dest => dest.BanTypeName, opt => opt.MapFrom(src => src.BanType.BanTypeName));
+
+            // DTO -> Entity
+            CreateMap<BanDTO, Ban>()
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.BanTypeId, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore())
+                .ForMember(dest => dest.BanType, opt => opt.Ignore())
+                .ForMember(dest => dest.BanReason, opt => opt.MapFrom(src => src.BanReason))
+                .ForMember(dest => dest.BanDate, opt => opt.MapFrom(src => src.BanDate))
+                .ForMember(dest => dest.BanDuration, opt => opt.MapFrom(src => src.BanDuration))
+                .ForMember(dest => dest.BanType.BanTypeName, opt => opt.MapFrom(src => src.BanTypeName));
+        }
+    }
+}
