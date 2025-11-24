dcd80af80da8a6987a025c94df9d8368168d50ab
Rselmane <rayan.selmane@etu.univ-smb.fr>
2025-11-24 17:41:53 +0100

    BanController fix

--- S5_01_App_CS_GOAT/Controllers/BanController.cs
+++ S5_01_App_CS_GOAT/Controllers/BanController.cs
@@ -1,68 +1,88 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using S5_01_App_CS_GOAT.DTO;
 using S5_01_App_CS_GOAT.Models.EntityFramework;
 using S5_01_App_CS_GOAT.Models.Repository;
+using S5_01_App_CS_GOAT.Services;
 
 namespace S5_01_App_CS_GOAT.Controllers
 {
     [Route("api/bans")]
     [ApiController]
-    public class BanController(/*IMapper mapper,*/ IDataRepository<Ban, int, string> manager, CSGOATDbContext context) : ControllerBase
+    public class BanController(IMapper mapper, IDataRepository<Ban, int, string> manager, CSGOATDbContext context) : ControllerBase
     {
-        [HttpGet("details/{id}")]
+        [HttpGet("ByUser")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Get(int id)
+        public async Task<ActionResult<IEnumerable<BanDTO>>> GetByUser()
         {
-            Ban? ban = await manager.GetByIdAsync(id);
-            return ban == null ? NotFound() : Ok(ban);
+            AuthResult authResult = JwtService.Authorized(null);
+            int? id = authResult.AuthUserId;
+
+            IEnumerable<Ban?> bans = await manager.GetAllAsync();
+            IEnumerable<Ban?> userBans = bans.Where(p => p.DependantUserId == id);
+
+            if (userBans == null || !userBans.Any())
+                return NotFound();
+            IEnumerable<BanDTO> userBansDTO = mapper.Map<IEnumerable<BanDTO>>(userBans);
+            return Ok(userBansDTO);
         }
 
         [HttpGet("all")]
-        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<IEnumerable<Ban>>> GetAll()
+        public async Task<ActionResult<IEnumerable<BanDTO>>> GetAll()
         {
-            IEnumerable<Ban?> ban = await manager.GetAllAsync();
-            if (ban == null || !ban.Any())
+            IEnumerable<Ban?> bans = await manager.GetAllAsync();
+
+            if (bans == null || !bans.Any())
                 return NotFound();
-            return Ok(ban);
+
+            // Mapping vers DTO
+            IEnumerable<BanDTO> bansDTO = mapper.Map<IEnumerable<BanDTO>>(bans);
+            return Ok(bansDTO);
         }
 
         [HttpPost("create")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Create(Ban ban)
+        public async Task<IActionResult> Create(BanDTO banDTO)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
+            // Mapping DTO vers entité
+            Ban ban = mapper.Map<Ban>(banDTO);
             await manager.AddAsync(ban);
 
-            return CreatedAtAction("Get", new { id = ban.UserId }, ban);
+            BanDTO createdBanDTO = mapper.Map<BanDTO>(ban);
+            return CreatedAtAction("GetAll", new { id = ban.UserId }, createdBanDTO);
         }
 
         [HttpPut("update/{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Update(int id, Ban ban)
+        public async Task<IActionResult> Update(int id, BanDTO banDTO)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-            Ban? banToUpdaten = await manager.GetByIdAsync(id);
 
-            if (banToUpdaten == null)
+            Ban? banToUpdate = await manager.GetByIdAsync(id);
+            if (banToUpdate == null)
             {
                 return NotFound();
             }
-            await manager.UpdateAsync(banToUpdaten, ban);
+
+            // Mapping DTO vers entité
+            Ban ban = mapper.Map<Ban>(banDTO);
+            await manager.UpdateAsync(banToUpdate, ban);
 
             return NoContent();
         }
-
     }
 }
