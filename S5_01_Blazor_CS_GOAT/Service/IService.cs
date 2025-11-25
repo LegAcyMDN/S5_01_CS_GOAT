9afd36f65859980eef7555124b0634976ce9ab76
Ewan-Ponchaud <ewan.ponchaud@gmail.com>
2025-11-25 17:46:09 +0100

    Avancé sur le home blazor

---
+++ S5_01_Blazor_CS_GOAT/Service/IService.cs
@@ -0,0 +1,11 @@
+namespace S5_01_Blazor_CS_GOAT.Service;
+
+public interface IService<TEntity>
+{
+    Task<List<TEntity>?> GetAllAsync();
+    Task<TEntity?> GetByIdAsync(int id);
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity updatedEntity);
+    Task DeleteAsync(int id);
+    Task<TEntity?> GetByNameAsync(string name);
+}
