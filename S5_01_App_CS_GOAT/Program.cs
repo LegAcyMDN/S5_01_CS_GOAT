f77cc5627b2c67d53700421cbc973cada9f8cad2
LegAcyMDN <ar9legacy.main@gmail.com>
2025-11-25 11:50:04 +0100

    Modification du Controller et Manager de Price History

--- S5_01_App_CS_GOAT/Program.cs
+++ S5_01_App_CS_GOAT/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
 using System.Text.Encodings.Web;
 using Microsoft.Extensions.Options;
+using S5_01_App_CS_GOAT.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -40,6 +41,7 @@ builder.Services.AddScoped<IWearRelatedRepository<Wear>, WearManager>();
 builder.Services.AddScoped<IDataRepository<UserNotification, int, string>, UserNotificationManager>();
 builder.Services.AddScoped<IDataRepository<Limit, int, (int, int)>, LimitManager>();
 builder.Services.AddScoped<IDataRepository<PromoCode, int, string>, PromoCodeManager>();
+builder.Services.AddScoped<IDataRepository<PriceHistory, int, string>, PriceHistoryManager>();
 
 builder.Services.AddDbContext<CSGOATDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("RemoteConnectionString")));
