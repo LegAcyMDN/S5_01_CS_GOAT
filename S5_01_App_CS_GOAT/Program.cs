c57283dc0618d4f118b04a6a1e40eeb708bca94f
RobinMILLET <millerob@etu.univ-smb.fr>
2025-11-28 11:24:17 +0100

    Ajout de GetAllNotificationType et de la chaîne SOLID associée

--- S5_01_App_CS_GOAT/Program.cs
+++ S5_01_App_CS_GOAT/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddScoped<ISkinRelatedRepository<Skin>, SkinManager>();
 builder.Services.AddScoped<IDataRepository<PriceHistory, int, string>, PriceHistoryManager>();
 builder.Services.AddScoped<IDataRepository<Notification, int, string>, NotificationManager>();
 builder.Services.AddScoped<INotificationRepository<Notification>, NotificationManager>();
+builder.Services.AddScoped<IDataRepository<NotificationType, int, string>, NotificationTypeManager>();
 builder.Services.AddScoped<IDataRepository<UpgradeResult, int, string>, UpgradeResultManager>();
 builder.Services.AddScoped<IDataRepository<ItemTransaction, int, string>, ItemTransactionManager>();
 
