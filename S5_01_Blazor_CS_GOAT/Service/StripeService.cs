f87e8b7c9a41bcf82482cdc71f96ecf180893a97
jmulins <ewan.ponchaud@gmail.com>
2025-12-31 20:51:54 +0100

    hotfix du hotfix

--- S5_01_Blazor_CS_GOAT/Service/StripeService.cs
+++ S5_01_Blazor_CS_GOAT/Service/StripeService.cs
@@ -5,7 +5,7 @@ public class StripeService
 {
     private readonly HttpClient _httpClient;
     private readonly AuthService _authService;
-    private readonly string _appURL = "https://apicsgoat-h7bhhpd4e7bnc9bh.eastus-01.azurewebsites.net";
+    private readonly string _appURL = "blazorcsgoat-hpbpdkhmadduekef.eastus-01.azurewebsites.net";
 
     public StripeService(HttpClient httpClient, AuthService authService)
     {
