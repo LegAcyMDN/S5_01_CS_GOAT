ce653e81ab9fe2f92cba4f58f89de5d8dd01ae88
LegAcyMDN <ar9legacy.main@gmail.com>
2026-01-12 11:14:08 +0100

    Modification du chemin de requête pour la modification du mot de passe.

--- S5_01_Blazor_CS_GOAT/ViewModels/ProfileViewModel.cs
+++ S5_01_Blazor_CS_GOAT/ViewModels/ProfileViewModel.cs
@@ -295,7 +295,7 @@ namespace S5_01_Blazor_CS_GOAT.ViewModels
 
                 _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-                var response = await _httpClient.PostAsJsonAsync("User/change-password", PasswordModel);
+                var response = await _httpClient.PatchAsJsonAsync("User/update", PasswordModel);
                 
                 if (response.IsSuccessStatusCode)
                 {
