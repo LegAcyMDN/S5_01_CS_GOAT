c605dd8f0d0ec67bf34b9ed4b8f459ccefbcda7e
robinmillet <millerob@etu.univ-smb.fr>
2026-01-13 17:42:24 +0100

    Création de la preview de Upgrade et d'un Skin getall pour l'upgrade. Manque l'implémentation pour l'exécution d'upgrade (avec fairrandom)

--- S5_01_App_CS_GOAT/Mapper/SkinMapper.cs
+++ S5_01_App_CS_GOAT/Mapper/SkinMapper.cs
@@ -10,6 +10,7 @@ public class SkinMapper : Profile
     {
         // Entity -> DTO
         CreateMap<Skin, SkinDTO>()
+            .ForMember(dest => dest.SkinId, opt => opt.MapFrom(src => src.SkinId))
             .ForMember(dest => dest.SkinName, opt => opt.MapFrom(src => src.SkinName))
             .ForMember(dest => dest.ItemName, opt => opt.MapFrom(src => src.Item.ItemName))
             .ForMember(dest => dest.RarityName, opt => opt.MapFrom(src => src.Rarity.RarityName))
---
+++ Shared/DTO/Helpers/UpgradeOutputDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTO.Helpers
+{
+    public class UpgradeOutputItemDTO
+    {
+        public InventoryItemDTO InventoryItem { get; set; } = new InventoryItemDTO();
+        public UpgradeResultDTO UpgradeResult { get; set; } = new UpgradeResultDTO();
+    }
+
+    public class UpgradeOutputDTO
+    {
+        public List<UpgradeOutputItemDTO> Items { get; set; } = new List<UpgradeOutputItemDTO>();
+
+        public InventoryItemDetailDTO? ItemResult { get; set; }
+
+        public UpgradeResultDTO? UpgradeResult { get; set; }
+    }
+}
